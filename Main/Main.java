f950c71725cb3f39d66b0a5e89cfbc6190cedafd
Jonorsky <jonavarrete@gbox.adnu.edu.ph>
2018-12-23 00:18:23 +0800

    Added new topics

--- Main/Main.java
+++ Main/Main.java
@@ -1,41 +1,56 @@
-import java.util.ArrayList;
-import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
 
 public class Main{
-
-  public static void print(ArrayList<Integer> arr){
-    for(Integer value: arr){
-      System.out.println(value);
-    }
-  }
-
   public static void main(String[] args){
-    // template classes = ArrayList<>
-    // we can put primitive type in ArrayList
-    ArrayList<Integer> numbers = new ArrayList<Integer>();
-    numbers.add(10);
-    numbers.add(100);
-    numbers.add(40);
 
-    for(int i=0; i<numbers.size(); i++)
-      System.out.println(numbers.get(i));
+    // HashSet does not retain order.
+    // only stores unique element
+    //Set<String> set1 = new HashSet<String>();
 
-    System.out.println();
+    // LinkedHashSet remembers the order we added items in
+    // Set<String> set1 = new LinkedHashSet<String>();
 
-    // Delete last element - reasonably fast
-    numbers.remove(numbers.size()-1);
-    print(numbers);
+    // TreeSet sorts in natural order/dictionary
+    Set<String> set1 = new TreeSet<String>();
 
-    // Delete first element - very slow, even middle of the list
-    numbers.remove(0);
 
-    // Solution is LinkedList
+    set1.add("dog");
+    set1.add("cat");
+    set1.add("mouse");
+    set1.add("snake");
+    set1.add("bear");
 
-    System.out.println();
-    print(numbers);
+    // Adding duplicate items does nothing.
+    set1. add("mouse");
 
-    List<String> values = new ArrayList<String>();
+    System.out.println(set1);
 
-    // LinkedList consumed more memory than arraylist: Pros and cons
+    /////////// Iteration //////////
+    for(String element: set1){
+      System.out.println(element);
+    }
+
+
+    // Intersection
+    Set<String> set2 = new TreeSet<String>();
+    set2.add("dog");
+    set2.add("cat");
+    set2.add("giraffe");
+    set2.add("monkey");
+    set2.add("ant");
+
+    Set<String> intersection = new HashSet<String>(set1);
+    System.out.println(intersection);
+
+    // Intersection between 2 sets
+    intersection.retainAll(set2);
+    System.out.println(intersection);
+
+    Set<String> difference = new HashSet<String>(set1);
+    difference .removeAll(set2);
+    System.out.println(difference);
   }
 }
